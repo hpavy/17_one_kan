6fc30e5d4e4c091468a2d59556da06e394811a29
hugopavy <hugo.pavy@etu.minesparis.psl.eu>
2024-11-21 13:45:49 +0000

    piche

--- model.py
+++ model.py
@@ -296,34 +296,32 @@ class KANLinear(torch.nn.Module):
 class KAN(torch.nn.Module):
     def __init__(
         self,
-        layers_hidden,
-        grid_size=5,
+        hyper_param,
         spline_order=3,
         scale_noise=0.1,
         scale_base=1.0,
         scale_spline=1.0,
         base_activation=torch.nn.SiLU,
         grid_eps=0.02,
-        grid_range=[-1, 1],
     ):
         super(KAN, self).__init__()
-        self.grid_size = grid_size
+        self.grid_size = hyper_param['grid_size']
         self.spline_order = spline_order
 
         self.layers = torch.nn.ModuleList()
-        for in_features, out_features in zip(layers_hidden, layers_hidden[1:]):
+        for in_features, out_features in zip(hyper_param['hiden_layer'], hyper_param['hiden_layer'][1:]):
             self.layers.append(
                 KANLinear(
                     in_features,
                     out_features,
-                    grid_size=grid_size,
+                    grid_size=hyper_param['grid_size'],
                     spline_order=spline_order,
                     scale_noise=scale_noise,
                     scale_base=scale_base,
                     scale_spline=scale_spline,
                     base_activation=base_activation,
                     grid_eps=grid_eps,
-                    grid_range=grid_range,
+                    grid_range=hyper_param['grid_range'],
                 )
             )
 
